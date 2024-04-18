0327494388e092f03f52241969ad48fe4d844780
Memelli <julio.memelli@gmail.com>
2024-04-18 02:04:53 -0300

    adicionando provider do apollo, e criando apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import {client} from './graphql/client.ts'
+import { ApolloProvider } from '@apollo/client'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
