29c2d06b1416fbef37d5b979a5dfdc9f96aac069
Memelli <julio.memelli@gmail.com>
2024-04-18 16:39:55 -0300

    alterando porta do vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   preview: {
-    port: 3000,
+    port: 5000,
     strictPort: true,
   },
   server: {
-    port: 3000,
+    port: 5000,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:3000"
-  }
+    origin: 'http://0.0.0.0:5000',
+  },
 })
