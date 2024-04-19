f3d78f78828abea64d3916465094e5b7d842749f
Memelli <julio.memelli@gmail.com>
2024-04-18 21:53:16 -0300

    arrumando avisos do eslint

--- src/utils/pokemons.dto.ts
+++ src/utils/pokemons.dto.ts
@@ -1,24 +1,31 @@
-import type { IPokemon } from "../interfaces/pokemon"
-import type { IPokemonsQueryData } from "../interfaces/pokemons-query-data"
+import type { IPokemon } from '@/interfaces/pokemon'
+import type { IPokemonsQueryData } from '@/interfaces/pokemons-query-data'
 
-export const pokemonsDTO = ({ id, name, info }: IPokemonsQueryData['pokemons'][0]): IPokemon => {
+export const pokemonsDTO = ({
+  id,
+  name,
+  info,
+}: IPokemonsQueryData['pokemons'][0]): IPokemon => {
   return {
     id,
-    name: name,
+    name,
     types: Array.from(
       new Set(
         info.nodes
           .map((node) => node.types)
           .flat()
-          .map(({ type }) => type.name)
-      )
+          .map(({ type }) => type.name),
+      ),
     ),
     stats: info.nodes
       .map((node) => node.stats)
       .flat()
-      .reduce((obj, { base_stat, stat }) => {
-        obj[stat.name] = base_stat
-        return obj
-      }, {} as Record<string, number>),
+      .reduce(
+        (obj, { base_stat, stat }) => {
+          obj[stat.name] = base_stat
+          return obj
+        },
+        {} as Record<string, number>,
+      ),
   }
 }
