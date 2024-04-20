dc5507608002cc5d2eed09feb9362587df84e533
Memelli <julio.memelli@gmail.com>
2024-04-20 02:48:56 -0300

    adicionando componente de toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Toaster } from './components/ui/toaster'
 import Router from './utils/routers'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className="mb-8 h-20 w-full border text-black">TEste</div>
       <Router />
+      <Toaster />
     </>
   )
 }
