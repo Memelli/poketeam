696c95eaa9c3ea7f9933da07658362eb5661209b
Memelli <julio.memelli@gmail.com>
2024-04-20 01:12:44 -0300

    adicionando configuracoes extras para o shadcn/ui

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import './index.css'
 import { client } from './graphql/client.ts'
 import { ApolloProvider } from '@apollo/client'
 import App from './App.tsx'
+import PokemonProvider from './context/pokemon-context/PokemonProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ApolloProvider client={client}>
-    <App />
+    <PokemonProvider>
+      <App />
+    </PokemonProvider>
   </ApolloProvider>,
 )
