f7924579d80f27f5e398f482ae214a1ae88dbf99
Memelli <julio.memelli@gmail.com>
2024-04-20 04:06:17 -0300

    criando estilo para card baseado no tipo do time

--- src/components/poke-teams/poke-teams-card/TeamsCard.tsx
+++ src/components/poke-teams/poke-teams-card/TeamsCard.tsx
@@ -1,17 +1,37 @@
 import { Card } from '@/components/ui/card'
+import { TeamTheme } from '@/utils/team-theme'
+import clsx from 'clsx'
 import { Link } from 'react-router-dom'
 
 interface ITeamsCard {
   id: number
   name: string
+  description: string
+  theme: string
   createdAt: Date
 }
 
-export default function TeamsCard({ id, name }: ITeamsCard) {
+export default function TeamsCard({
+  id,
+  name,
+  description,
+  theme,
+}: ITeamsCard) {
   return (
     <Link to={`/my-teams/${id}`}>
-      <Card className="flex h-20 flex-col items-center justify-center">
+      <Card
+        className={clsx(
+          'flex flex-col items-center justify-center py-2',
+          TeamTheme[theme as keyof typeof TeamTheme].border,
+        )}
+      >
+        <img
+          alt="Imagem dos times com as cores relacionadas ao mesmo"
+          className="h-20 w-20"
+          src={`${window.location.origin}/assets/teams/${TeamTheme[theme as keyof typeof TeamTheme].bgImage}`}
+        />
         <p className="font-bold">{name}</p>
+        <p className="text-sm font-light italic">{description}</p>
       </Card>
     </Link>
   )
---
+++ src/utils/team-theme.ts
@@ -0,0 +1,14 @@
+export const TeamTheme = {
+  red: {
+    bgImage: 'TeamValor.webp',
+    border: 'border-red-500',
+  },
+  yellow: {
+    bgImage: 'TeamInstinct.webp',
+    border: 'border-yellow-500',
+  },
+  blue: {
+    bgImage: 'TeamMystic.webp',
+    border: 'border-blue-500',
+  },
+}
