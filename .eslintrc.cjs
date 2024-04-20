aa8b236e0242276498e3993b3c31083ade9253be
Memelli <julio.memelli@gmail.com>
2024-04-20 02:46:23 -0300

    consertando erro de plugin do eslint-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,12 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    "@rocketseat/eslint-config/react",
     'eslint:recommended',
+    'plugin:prettier/recommended',
+    'prettier',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    "@rocketseat/eslint-config/react"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
