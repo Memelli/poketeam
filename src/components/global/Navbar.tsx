083a504350268508607b37bf8a36d2e21683eae4
Memelli <julio.memelli@gmail.com>
2024-04-21 11:14:51 -0300

    criando componente NavbarLink

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,12 +1,19 @@
+function NavbarLink({ to, name }: { to: string; name: string }) {
+  return (
+    <a
+      href={to}
+      className="rounded-sm px-3 py-2 hover:bg-blue-200 hover:bg-opacity-40 hover:font-extrabold "
+    >
+      {name}
+    </a>
+  )
+}
+
 export default function Navbar() {
   return (
     <div className="flex gap-x-4 text-lg font-bold tracking-tight text-white">
-      <a href="/" className="hover:font-extrabold hover:underline">
-        Home
-      </a>
-      <a href="/my-teams/" className="hover:font-extrabold hover:underline">
-        Meus Times
-      </a>
+      <NavbarLink to="/" name="Home" />
+      <NavbarLink to="/my-teams" name="Meus times" />
     </div>
   )
 }
